2d4a1ceeb76c6ad3794ae6aa379788489086efc7
Abner Rodriguez <abner07282005@gmail.com>
2025-05-24 14:56:00 -0400

    Initial Commit

---
+++ spring-gym-api/src/main/java/practice/spring_gym_api/SpringGymApiApplication.java
@@ -0,0 +1,13 @@
+package practice.spring_gym_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGymApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGymApiApplication.class, args);
+	}
+
+}
---
+++ spring-gym-api/src/test/java/practice/spring_gym_api/SpringGymApiApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.spring_gym_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGymApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
